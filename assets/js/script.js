66b36a828748355043d60ea0b714b7f188f297e5
jbairoso <jbairoso@gmail.com>
2022-09-05 19:02:14 -0700

    fix issue w/ pw not generating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 // Assignment code here
-var lowercase =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var LowerCase =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var UpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 var passwordLength = " ";
 var characterTypes = " ";
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 function generatePassword(){
   var passwordLength = (prompt("Please select how many characters you would like in your password. From 8 to 128 characters."));
@@ -21,18 +22,19 @@ function generatePassword(){
 }if (confirmLowerCase === false && confirmUpperCase === false && confirmNumbers === false && confirmSpecialCharacters === false) {
   alert("You must pick at least one character type.");
 }if (confirmLowerCase === true) {
-  characterTypes = characterTypes.concat(lowerCase);
+  characterTypes = characterTypes.concat(LowerCase);
 }if (confirmUpperCase === true) {
-  characterTypes = characterTypes.concat(upperCase);
+  characterTypes = characterTypes.concat(UpperCase);
 }if (confirmNumbers === true) {
   characterTypes = characterTypes.concat(numbers);
 }if (confirmSpecial === true) {
   characterTypes = characterTypes.concat(special);
 } 
 
- var passwordText = " "
- for (var i = 0; i < passwordLength; i++) {
-  passwordText = passwordText + characterTypes[Math.floor(Math.random() * characterTypes.length)];
+var passwordText = " "
+for (var i = 0; i < passwordLength; i++) {
+ passwordText = passwordText + characterTypes[Math.floor(Math.random() * characterTypes.length)];
+  // console.log(password)
 } return passwordText; 
 
 }
