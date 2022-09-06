2d793b6c68a59c63c38661e17fde663abd782f41
jbairoso <jbairoso@gmail.com>
2022-09-05 19:04:14 -0700

    clean ap array

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var LowerCase =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var UpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var special = [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
 var passwordLength = " ";
 var characterTypes = " ";
 
@@ -34,7 +34,6 @@ function generatePassword(){
 var passwordText = " "
 for (var i = 0; i < passwordLength; i++) {
  passwordText = passwordText + characterTypes[Math.floor(Math.random() * characterTypes.length)];
-  // console.log(password)
 } return passwordText; 
 
 }
